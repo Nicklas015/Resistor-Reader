e78b19861ca4650c4dd5aa702946d693e379e863
nicklas015 <nicklas.b15@gmail.com>
2026-02-15 19:22:33 +0100

    working cross compile

--- toolchains/rpi-zero.cmake
+++ toolchains/rpi-zero.cmake
@@ -1,37 +1,24 @@
-# ----------------------------------------
-# Raspberry Pi Zero (ARMv6 hard-float)
-# ----------------------------------------
-
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_CROSSCOMPILING TRUE)
-set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+# Sti til Bootlin Toolchain
+set(TOOLCHAIN_PATH $ENV{HOME}/armv6-eabihf--glibc--stable-2025.08-1)
+set(CMAKE_C_COMPILER ${TOOLCHAIN_PATH}/bin/arm-linux-gcc)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PATH}/bin/arm-linux-g++)
 
-# Sysroot
-set(CMAKE_SYSROOT $ENV{HOME}/Resistor-Reader/rootfs)
-set(CMAKE_FIND_ROOT_PATH ${CMAKE_SYSROOT})
+# Din Raspberry Pi rootfs (kun til biblioteker)
+set(PI_ROOTFS $ENV{HOME}/Resistor-Reader/rootfs)
 
-# Tell CMake where to search packages
-set(CMAKE_PREFIX_PATH
-    ${CMAKE_SYSROOT}/usr
-    ${CMAKE_SYSROOT}/usr/lib/arm-linux-gnueabihf/cmake/opencv4
-)
+# Brug Bootlin som primær sysroot for at sikre korrekte C++ headers
+set(CMAKE_SYSROOT ${TOOLCHAIN_PATH}/arm-buildroot-linux-gnueabihf/sysroot)
 
-# Compilers
-set(TOOLCHAIN_PATH $ENV{HOME}/armv6-eabihf--glibc--stable-2024.05-1)
-set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
-
-# Force ARMv6 + ARM mode
-# Ret denne linje i din toolchain fil:
-set(PI_ZERO_FLAGS "-mcpu=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard -marm")
-
-
+# Flag til Raspberry Pi Zero
+set(PI_ZERO_FLAGS "-mcpu=arm1176jzf-s -mfloat-abi=hard -mfpu=vfp -marm")
 set(CMAKE_C_FLAGS_INIT "${PI_ZERO_FLAGS}")
 set(CMAKE_CXX_FLAGS_INIT "${PI_ZERO_FLAGS}")
 
-# Search behavior
+# Søge-adfærd
+set(CMAKE_FIND_ROOT_PATH ${PI_ROOTFS} ${CMAKE_SYSROOT})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
